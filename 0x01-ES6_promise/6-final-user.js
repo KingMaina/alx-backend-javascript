aded52fb943d251e722c03356c3031d8c89766ad
Andrew <drew29799@gmail.com>
2024-03-08 22:38:31 +0000

    Fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   const results = Promise.allSettled(promises)
     .then((data) => data.map((result) => {
-      if (result.status === 'rejected') return ({ status: result.status, value: result.reason });
+      if (result.status === 'rejected') return ({ status: result.status, value: String(result.reason) });
       return ({ status: result.status, value: result.value });
     }));
   return results;
