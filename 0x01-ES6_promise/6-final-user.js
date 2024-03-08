ffc40e053379b7af34a74d608ff4a9ca6c110649
Andrew <drew29799@gmail.com>
2024-03-08 22:10:44 +0000

    Fix linting issues
    * Fixed issues where promises were not returned

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,11 +4,9 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   const results = Promise.allSettled(promises)
-    .then((data) => {
-      return data.map((result) => {
-        if (result.status === "rejected") return ({status: result.status, value: String(result.reason)});
-        return ({status: result.status, value: result.value});
-      })
-    });
+    .then((data) => data.map((result) => {
+      if (result.status === 'rejected') return ({ status: result.status, value: result.reason });
+      return ({ status: result.status, value: result.value });
+    }));
   return results;
 }
