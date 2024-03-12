99315f0af5e0ea7cbec63c5cf7a1b94e7770245c
Andrew <drew29799@gmail.com>
2024-03-12 21:51:47 +0000

    Fix count issue

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -7,7 +7,7 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 1);
-  } else if (weakMap.get(endpoint) >= 5) {
+  } else if (weakMap.get(endpoint) >= 4) {
     throw new Error('Endpoint load is high');
   } else {
     const timesAPIQueried = weakMap.get(endpoint);
